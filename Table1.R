a05c210a9a798c02214a9b81de2276482b8017b1
rborchering <rborchering@ufl.edu>
2013-08-21 11:18:39 -0400

    Upload Table1 file
    Group file for working on table 1:
    * Commented section for relating elements in the table to their
    corresponding equations/parameters in the text.
    * Place to begin coding the equations.

---
+++ Table1.R
@@ -0,0 +1,24 @@
+# File for working on the equations in Table 1 of Morin-Comrie 2010.
+# 
+# Corresponding equations/parameters in the text:
+#   laid = Et, eqn_8 ?
+#   DeathEgg = eqn_10 ?
+#   Egg to IS1 = Vt?
+#   IS1 to IS2 =  
+#   IS2 to IS3 = 
+#   IS3 to IS4 =
+#   IS4 to Pup =
+#   Pup to Adult M =    , Pup to Adult F = 
+#   DeathIS1 =
+#   DeathIS2 =  
+#   DeathIS3 =
+#   DeathIS4 =
+#   DeathPup =
+#   Death Male =    , Death Female = 
+#   Precip =    , Precip2 =
+#   Irrigation =    , Irrigation2 =   
+#   Evaporation = 
+#   Infiltration =
+#   Spill = 
+  
+# Model functions:
